89827dfe5f582347629c5ca204756006f6f3abdf
iotcat <i@iotcat.me>
2020-02-03 16:40:43 +0000

    to push

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ var sms = (url) => {
 
 	var send = (to, msg0, msg1) => {
 		return new Promise((resolve, reject) => {
-			request(url + `?to=${to}&s0=${msg0}&s1=${msg1}&t=${Date.parse(new Date())/1000}`, (err, res, body) => {
+			request.post({
+				"url": url,
+				formData: {
+					 "to": to,
+					 "s0": msg0,
+					 "s1": msg1,
+					 "t": Date.parse(new Date())/1000
+				}
+			}, (err, res, body) => {
 				resolve(body);
 			});
 		});
